4e92aef7086b468f15bd385eee69998482aaa519
Alex Orga <alexorga.dev@gmail.com>
2018-03-24 13:45:52 -0500

    input function and eventlisteners are finished

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.log('hi');
 
-var languages = [
-    // Spanish
-    {
+var spanish = {
         "a": "un",
         "an": "un",
         "the": "el",
@@ -22,9 +20,8 @@ var languages = [
         "warm": "calentar",
         "wishes": "deseos",
         "kwanzaa": "kwanzaa"
-    },
-    // German
-    {
+    };
+var german = {
         "a": "ein",
         "an": "ein",
         "the": "das",
@@ -44,9 +41,8 @@ var languages = [
         "warm": "warm",
         "wishes": "wunsch",
         "kwanzaa": "kwanzaa"
-    },
-    // Klingon
-    {
+    };
+var klingon = {
         "a": ".",
         "an": "ghah",
         "the": ".",
@@ -66,5 +62,82 @@ var languages = [
         "warm": "warm",
         "wishes": "vlneh",
         "kwanzaa": "kwanzaa"
-    }
-];
+    };
+
+// var spanish = document.getElementById("spanishBtn");
+// var array = ["merry", "christmas"];
+// var span = languages[0];
+
+// for(var i=0; i<span.length; i++){
+//     var string = span[i];
+//     console.log(string);
+// }
+
+// spanish.addEventListener("click", function getInput(){
+//     var input = document.getElementById("textArea").value;
+//     var splitInput = input.split(' ');
+//     console.log(splitInput);
+// });
+
+// document.getElementById("germanBtn").addEventListener("click", );
+
+// document.getElementById("klingonBtn").addEventListener("click", );
+
+// function output(id){
+//     var outputString = document.getElementById(id).value;
+//     outputString.innerHTML = "<p>" + + "</p>";
+// }
+
+// *************************IN-CLASS**************************
+
+// NEED SEPARATE OBJECTS FOR EACH LANGUAGE NOT ARRAY OF OBJECTS
+
+const inputBox = document.getElementById("textArea");
+const spanishBtn = document.getElementById("spanishBtn");
+const outputBox = document.getElementById('output');
+
+// spanishBtn.addEventListener('click', () => {
+//     const userInput = inputBox.value.toLowerCase();
+//     let domOutput = spanish[userInput];
+//     outputBox.innerHTML = domOutput;
+// })
+
+// const allMyButtons = document.getElementsByClassName('translator');
+
+// for (let i=0; i<allMyButtons.length; i++){
+//     allMyButtons[i].addEventListener('click', (e) => {
+//         const userInput = inputBox.value.toLowerCase();
+//         let domOutput = '';
+//         if (e.target.id === 'spanish') {
+//             domOutput = spanish[userInput];
+//         } else if (e.target.id === 'french'){
+//             domOutput = french[userInput];
+//         } else if( e.target.id === 'german'){
+//             domOutput = german[userInput];
+//         }
+//         outputBox.innerHTML = domOutput;
+//     })
+// }
+
+const allMyButtons = document.getElementsByClassName('translator');
+
+for (let i=0; i<allMyButtons.length; i++){
+    allMyButtons[i].addEventListener('click', (e) => {
+        const userInput = inputBox.value.toLowerCase().split(' ');
+        let domOutput = [];
+        if (e.target.id === 'spanishBtn') {
+            userInput.forEach((word) => {
+                domOutput.push(spanish[word])
+            });
+        } else if (e.target.id === 'germanBtn'){
+            userInput.forEach((word) => {
+                domOutput.push(german[word])
+            });
+        } else if( e.target.id === 'klingonBtn'){
+            userInput.forEach((word) => {
+                domOutput.push(klingon[word])
+            });
+        }
+        outputBox.innerHTML = domOutput.join(' ');
+    })
+}
